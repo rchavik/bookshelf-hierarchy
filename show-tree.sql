4c086a0d71d49ae2ff2e9136ac9416206d450315
Rachman Chavik <rchavik@xintesa.com>
2016-09-24 10:24:51 +0700

    Rename files for conversion as mocha test

--- show-tree.sql
+++ show-tree.sql
@@ -3,7 +3,7 @@ SELECT node.id, CONCAT( REPEAT('  ', COUNT(parent.name) - 1), node.name) AS name
   FROM nested_category AS node,
        nested_category AS parent
  WHERE node.lft BETWEEN parent.lft AND parent.rgt
-   AND parent_section_id = node.section_id
+   AND parent.section_id = node.section_id
  GROUP BY node.section_id, node.name, node.lft, node.id
  ORDER BY node.section_id, node.lft;
 
